d29f936a4040c974ccfdac9dd81a064a87030c04
xaxaxaxakk <100176511+xaxaxaxakk@users.noreply.github.com>
2025-11-29 01:07:20 +0900

    수정

--- etc.js
+++ etc.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
     $("#" + tabId).addClass("active");
   });
   $(".tab-btn").first().click();
+
+  $("#custom-font-color > h4 > span").on("click", function () {
+    const CFC = $("#custom-font-color");
+    const lists = CFC.find(".font-color-lists");
+    const toggleButton = $(this);
+    lists.slideUp();
+
+    if (CFC.hasClass("hide")) {
+      lists.stop().slideDown(200);
+      CFC.removeClass("hide").addClass("opened");
+      toggleButton.text("닫기");
+    } else {
+      lists.stop().slideUp(200);
+      CFC.removeClass("opened").addClass("hide");
+      toggleButton.text("열기");
+    }
+  });
 });
