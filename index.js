4beff0962952906e477e551fabdb860dbbdacf84
xaxaxaxakk <100176511+xaxaxaxakk@users.noreply.github.com>
2026-09-11 14:09:12 +0900

    1.5.1

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ const defaultSettings = {
     fontFamily: "Pretendard-Regular",
     fontWeight: "normal",
     htmlFontFace: "Ridibatang",
-    fontSize: 32,
-    fontSizeImage: 32,
+    fontSize: 38,
+    fontSizeImage: 38,
     fontSizeHtml: 14,
     fontSpacing: 0,
     fontLineHeight: 1.5,
@@ -531,10 +531,10 @@ function resolveMetaText(mode, customValue) {
 const WATERMARK_MARK = "READER.";
 const SHARE_REFERENCE_WIDTH = 1080;
 const SHARE_LAYOUT = {
-    paddingTop: 130,
-    paddingBottom: 130,
-    paddingBottomMeta: 160,
-    metaGap: 24,
+    paddingTop: 200,
+    paddingBottom: 200,
+    paddingBottomMeta: 200,
+    metaGap: 0,
 };
 function getMetaMetrics(width) {
     const scale = width / SHARE_REFERENCE_WIDTH;
